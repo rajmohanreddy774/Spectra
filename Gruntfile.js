fd19f9a48d3ee579ac32d1e98c475d303abc8bc9
Abhijit Nage <47779685+AbhijitNage123@users.noreply.github.com>
2026-01-29 23:31:09 +0530

    Grunt release setup (#3)

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,68 @@ module.exports = function ( grunt ) {
 	grunt.initConfig( {
 		pkg: grunt.file.readJSON( 'package.json' ),
 
+		copy: {
+			main: {
+				options: {
+					mode: true,
+				},
+				src: [
+					'**',
+					'!node_modules/**',
+					'!.git/**',
+					'!*.sh',
+					'!*.zip',
+					'!eslintrc.json',
+					'!README.md',
+					'!Gruntfile.js',
+					'!package.json',
+					'!package-lock.json',
+					'!.gitignore',
+					'!.gitattributes',
+					'!*.zip',
+					'!*.neon',
+					'!Optimization.txt',
+					'!composer.json',
+					'!composer.lock',
+					'!phpcs.xml.dist',
+					'!phpunit.xml.dist',
+					'!vendor/**',
+					'!src/**',
+					'!scripts/**',
+					'!config/**',
+					'!tests/**',
+					'!bin/**',
+					'!webpack.config.js',
+					'!auth.json',
+					'!spectra/node_modules/**',
+					'!spectra/src/**',
+					'!spectra/composer.json',
+					'!spectra/composer.lock',
+					'!spectra/package.json',
+					'!spectra/package-lock.json',
+					'!spectra/README.md',
+				],
+				dest: 'spectra/',
+			},
+		},
+		compress: {
+			main: {
+				options: {
+					archive:
+						'spectra-<%= pkg.version %>.zip',
+					mode: 'zip',
+				},
+				files: [
+					{
+						src: ['./spectra/**'],
+					},
+				],
+			},
+		},
+		clean: {
+			main: ['spectra'],
+			zip: ['*.zip'],
+		},
 		makepot: {
 			target: {
 				options: {
@@ -37,6 +99,15 @@ module.exports = function ( grunt ) {
 	} );
 
 	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
+	grunt.loadNpmTasks( 'grunt-contrib-compress' );
+	grunt.loadNpmTasks( 'grunt-contrib-clean' );
 
 	grunt.registerTask( 'i18n', [ 'addtextdomain', 'makepot' ] );
+	grunt.registerTask( 'release', [
+		'clean:zip',
+		'copy',
+		'compress',
+		'clean:main',
+	] );
 };
